0050cba17def0638500299a68a655beb913babaa
Ramanan-Bala <58137577+Ramanan-Bala@users.noreply.github.com>
2023-02-26 01:11:58 +0530

    added skill section

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const cursor = document.getElementById("cursor");
 const cursorDot = document.getElementById("cursor-dot");
 const nav = document.getElementById("nav");
 const home = document.getElementById("home");
+const navItems = nav.querySelectorAll("a");
+const navIndicator = nav.querySelector("#indicator");
+const sections = document.querySelectorAll("section");
 
 window.addEventListener("load", function () {
   const loader = document.getElementById("loader");
@@ -25,6 +28,24 @@ window.onscroll = function (e) {
     var(--purple) ${100 - p}%
   )`;
   else topbar.style.background = "#2c2c2c";
+
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navItems.forEach((item, i) => {
+    item.classList.remove("selected");
+    if (item.href.includes(current)) {
+      item.classList.add("selected");
+      navIndicator.style.left = `${10 + 83 * i}px`;
+      navIndicator.setAttribute("pos", i);
+    }
+  });
 };
 
 let letters = ["R", "a", "m", "s", "H", "e", "r", "e"];
@@ -117,9 +138,6 @@ window.onpointermove = (e) => {
   cursorDot.animate(keyframe, option(1200));
 };
 
-let navItems = nav.querySelectorAll("a");
-let navIndicator = nav.querySelector("#indicator");
-
 function activeLink() {
   navItems.forEach((item) => item.classList.remove("selected"));
 }
@@ -138,4 +156,35 @@ navItems.forEach((item, i) => {
     navIndicator.style.left = `${10 + 83 * i}px`;
     navIndicator.setAttribute("pos", i);
   });
+});
+
+// !! skill
+
+const subtitles = document.querySelectorAll(".card-subtitle");
+
+const createWord = (text, index) => {
+  const word = document.createElement("li");
+
+  word.innerHTML = `${text} `;
+
+  word.classList.add("card-subtitle-word");
+
+  word.style.transitionDelay = `${index * 80}ms`;
+
+  return word;
+};
+
+subtitles.forEach((subtitle, i) => {
+  const addWord = (text, index) =>
+    subtitle.appendChild(createWord(text, index));
+
+  const createSubtitle = (text) => text.split(" ").map(addWord);
+  if (i == 0)
+    createSubtitle(
+      "HTML CSS SASS Javascript Typescript Node\tJS Angular React NextJS Material\tUI NgZorro Prime\tNg TailwindCSS Figma Responsive\tDesign"
+    );
+  if (i == 1)
+    createSubtitle(
+      "Python Java Typescript Django Spring\tBoot Node\tJS Express Sequelize Mongoose SQL MongoDb"
+    );
 });
